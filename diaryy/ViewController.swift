9dc932cd1804a24092d272f185d97ffa494748d5
jubomi <bomi050623@gmail.com>
2022-04-10 12:19:47 +0900

    [UPDATE] Diary

---
+++ diaryy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  diaryy
+//
+//  Created by 주보미 on 2022/04/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
